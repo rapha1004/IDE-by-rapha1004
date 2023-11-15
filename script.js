19d278d1044d6aaa6f67d0acc37838efbdf5c19a
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2023-11-15 17:36:39 +0100

    backup update

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ let CSS = document.querySelector("#CSS")
 let divSite = document.querySelector("#divSite")
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    reqbackup();
+});
+
+function reqbackup() {
+   let HTMLBackup = localStorage.getItem('htmlBackup')
+   let CSSBackup = localStorage.getItem('cssBackup')
+   HTML.value = HTMLBackup
+   CSS.value = CSSBackup
+}
+
 setInterval(() => {
-    let CSSAdd = "<style>"+ CSS.value+"</style>"
+    let CSSAdd = CSS.value
+    let write_css = "<style>"+ CSSAdd+"</style>"
     let HTMLAdd = HTML.value
     divSite.innerHTML = (CSSAdd + HTMLAdd)
+    localStorage.setItem('htmlBackup', HTMLAdd);
+    localStorage.setItem('cssBackup', CSSAdd);
 }, 200);
